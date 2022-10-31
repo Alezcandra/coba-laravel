5570ddc48ecfef702e16b7d58d310eb51fbefee3
Alezcandra <smithbradley9998@gmail.com>
2022-10-31 15:29:38 +0700

    commit

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('container')
+
+<h1 class="mb-5">{{ $post->title }}</h1>
+
+<p>By. Linggih Kusumah Dilagga in <a href="/categories/{{ $post->category->slug }}"{{ $post->category->name }}></a></p>
+
+{!! $post->body !!}    
+
+<a href="/blog">Back to Posts</a>
+
+@endsection
